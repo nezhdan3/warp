38ff245b9dfb5673361cfc64a220149b005f0a4d
nezhdan3 <negdan3@yandex.ru>
2026-07-25 07:54:02 +0300

    Add files via upload

---
+++ replit.nix
@@ -0,0 +1,13 @@
+{ pkgs }: {
+  deps = [
+    pkgs.bash
+    pkgs.wireguard-tools
+    pkgs.jq
+    pkgs.bashInteractive
+    pkgs.nodePackages.bash-language-server
+    pkgs.coreutils
+    pkgs.curl
+    pkgs.openssl
+    pkgs.qrencode
+  ];
+}
